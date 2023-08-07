597d1b38eccebc904f3ac033c266453e9e661b3b
notkrithiramnath <114512556+notkrithiramnath@users.noreply.github.com>
2023-08-06 21:43:57 -0400

    Create page.php

---
+++ page.php
@@ -0,0 +1,8 @@
+<?php
+$name = $_POST["name"];
+$message = $_POST["message"];
+$priority = $_POST["priority"];
+$type = $_POST["type"];
+$terms = $_POST["terms"];
+
+var_dump($name, $message, $priority,$type,$terms)
